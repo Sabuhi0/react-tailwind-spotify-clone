db88f5873832bb8587dfd02eb5dca2895fc6e60a
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-04 14:57:04 +0400

    Update Menu component

--- src/components/Sidebar/Menu/index.jsx
+++ src/components/Sidebar/Menu/index.jsx
@@ -1,10 +1,36 @@
+import { Icon } from '../../../assets/Icons'
 import React from 'react'
 
 const Menu = () => {
   return (
-    <ul>
-      <li></li>
-    </ul>
+    <nav className='px-2'>
+      <ul className='flex flex-col'>
+        <li>
+          <a href="#" className='h-10 flex gap-x-4 items-center text-sm font-semibold hover:text-white px-4 text-active'>
+            <span>
+              <Icon name="home" />
+            </span>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className='h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4'>
+            <span>
+              <Icon name="search" />
+            </span>
+            Search
+          </a>
+        </li>
+        <li>
+          <a href="#" className='h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4'>
+            <span>
+              <Icon name="collection" />
+            </span>
+            You Library
+          </a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
