cbf41152da233487a2cd6f89cdc318a8c42746a8
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-04 15:02:09 +0400

    Update Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Logo from '../../src/assets/icons/logo.svg'
+import Menu from './Sidebar/Menu';
 
 const Sidebar = () => {
   return (
-    <aside className='w-60'>
-      Sidebar
+    <aside className='w-60 py-6 flex flex-col'>
+      <a href="" className='mb-7 px-6'>
+        <img src={Logo} alt="Spotify" className='w-[131px]' />
+      </a>
+      <Menu />
     </aside>
   )
 }
