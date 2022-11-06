bb874b813882fd37ec5a47f7ea99aab2e2e149cb
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-06 21:19:42 +0400

    Added BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
 import Content from "./components/Content";
 import PlayingBar from "./components/PlayingBar";
 import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <div className="wrapper">
         <Sidebar />
         <Content />
       </div>
       <PlayingBar />
-    </>
+    </BrowserRouter>
   )
 }
 
