62b1c24adb79ccdbb2b24dd861106509d7530f18
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-07 16:42:05 +0400

    Add useLocation

--- src/components/Sidebar/Menu/index.jsx
+++ src/components/Sidebar/Menu/index.jsx
@@ -1,33 +1,38 @@
 import { Icon } from '../../../assets/Icons'
 import React from 'react'
+import { NavLink, useLocation } from 'react-router-dom';
 
 const Menu = () => {
+  // Assigning location variable
+  const location = useLocation()
+  // Javascript split method to get the name of the path in array
+  const splitLocation = location.pathname.split("/")
   return (
     <nav className='px-2'>
       <ul className='flex flex-col'>
         <li>
-          <a href="#" className='h-10 flex gap-x-4 items-center text-sm font-semibold hover:text-white px-4 text-active'>
+          <NavLink to="/" className={`${splitLocation[1] === "" ? "text-active" : ""} h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4`}>
             <span>
               <Icon name="home" />
             </span>
             Home
-          </a>
+          </NavLink>
         </li>
         <li>
-          <a href="#" className='h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4'>
+          <NavLink to="/search" className={`${splitLocation[1] === "search" ? "text-active" : ""} h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4`}>
             <span>
               <Icon name="search" />
             </span>
             Search
-          </a>
+          </NavLink>
         </li>
         <li>
-          <a href="#" className='h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4'>
+          <NavLink to="/collection" className={`${splitLocation[1] === "collection" ? "text-active" : ""} h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4`}>
             <span>
               <Icon name="collection" />
             </span>
             You Library
-          </a>
+          </NavLink>
         </li>
       </ul>
     </nav>
