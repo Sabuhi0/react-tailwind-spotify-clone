fc5ae67ee36866de0c42bc652e664c9d678fa480
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-07 16:29:57 +0400

    Add Routes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
 import Navbar from '../components/Navbar';
+import Home from '../views/Home';
+import Search from '../views/Search';
+import Collection from '../views/Collection';
 
 const Content = () => {
   return (
     <main className='flex-auto'>
       <Navbar />
-      Content
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/collection" element={<Collection />} />
+      </Routes>
     </main>
   )
 }
