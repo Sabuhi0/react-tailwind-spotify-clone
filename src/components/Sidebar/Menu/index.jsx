0b47636069d596b4f1c41cb5b4ff30748fa7d26b
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-07 23:15:26 +0400

    Update Menu component

--- src/components/Sidebar/Menu/index.jsx
+++ src/components/Sidebar/Menu/index.jsx
@@ -1,38 +1,42 @@
 import { Icon } from '../../../assets/Icons'
 import React from 'react'
-import { NavLink, useLocation } from 'react-router-dom';
+import { Link,  useLocation } from 'react-router-dom';
 
 const Menu = () => {
   // Assigning location variable
-  const location = useLocation()
+  const location = useLocation();
+
+  // Destructuring pathname from location
+  const { pathname } = location;
+  
   // Javascript split method to get the name of the path in array
-  const splitLocation = location.pathname.split("/")
+  const splitLocation = pathname.split("/");
   return (
     <nav className='px-2'>
       <ul className='flex flex-col'>
         <li>
-          <NavLink to="/" className={`${splitLocation[1] === "" ? "text-active" : ""} h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4`}>
+          <Link to="/" className={`${splitLocation[1] === "" ? "!text-active" : ""} h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4`}>
             <span>
               <Icon name="home" />
             </span>
             Home
-          </NavLink>
+          </Link>
         </li>
         <li>
-          <NavLink to="/search" className={`${splitLocation[1] === "search" ? "text-active" : ""} h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4`}>
+          <Link to="/search" className={`${splitLocation[1] === "search" ? "text-active" : ""} h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4`}>
             <span>
               <Icon name="search" />
             </span>
             Search
-          </NavLink>
+          </Link>
         </li>
         <li>
-          <NavLink to="/collection" className={`${splitLocation[1] === "collection" ? "text-active" : ""} h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4`}>
+          <Link to="/collection" className={`${splitLocation[1] === "collection" ? "text-active" : ""} h-10 flex gap-x-4 items-center text-sm text-link font-semibold hover:text-white px-4`}>
             <span>
               <Icon name="collection" />
             </span>
             You Library
-          </NavLink>
+          </Link>
         </li>
       </ul>
     </nav>
