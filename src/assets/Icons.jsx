4b6cfa78fd3625e5a6c73cf6196e001fb136f6e3
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-08 21:48:12 +0400

    Add fill

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -25,7 +25,7 @@ const CollectionIcon = ({ size }) => {
 const PlusIcon = ({ size }) => {
     return (
         <svg viewBox="0 0 16 16" height={size} width={size} xmlns="http://www.w3.org/2000/svg">
-            <path d="M15.25 8a.75.75 0 01-.75.75H8.75v5.75a.75.75 0 01-1.5 0V8.75H1.5a.75.75 0 010-1.5h5.75V1.5a.75.75 0 011.5 0v5.75h5.75a.75.75 0 01.75.75z" />
+            <path d="M15.25 8a.75.75 0 01-.75.75H8.75v5.75a.75.75 0 01-1.5 0V8.75H1.5a.75.75 0 010-1.5h5.75V1.5a.75.75 0 011.5 0v5.75h5.75a.75.75 0 01.75.75z" fill='currentColor' />
         </svg>
     )
 }
