5bfd15dcf5229612ed9a0febc5689bbdcf06e90a
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-10 22:27:50 +0400

    Update Icons component

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -32,7 +32,7 @@ const PlusIcon = ({ size }) => {
 
 const HeartIcon = ({ size }) => {
     return (
-        <svg role="img" height={size} width={size} aria-hidden="true" viewBox="0 0 16 16" class="Svg-ytk21e-0 eqtHWV">
+        <svg viewBox="0 0 16 16" height={size} width={size} xmlns="http://www.w3.org/2000/svg">
             <path d="M15.724 4.22A4.313 4.313 0 0012.192.814a4.269 4.269 0 00-3.622 1.13.837.837 0 01-1.14 0 4.272 4.272 0 00-6.21 5.855l5.916 7.05a1.128 1.128 0 001.727 0l5.916-7.05a4.228 4.228 0 00.945-3.577z" fill="currentColor" />
         </svg>
     )
@@ -40,7 +40,7 @@ const HeartIcon = ({ size }) => {
 
 const DownloadIcon = ({ size }) => {
     return (
-        <svg role="img" height={size} width={size} aria-hidden="true" viewBox="0 0 24 24" class="Svg-ytk21e-0 eqtHWV">
+        <svg viewBox="0 0 24 24" height={size} width={size} xmlns="http://www.w3.org/2000/svg">
             <path d="M12 3a9 9 0 100 18 9 9 0 000-18zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z" />
             <path d="M12 6.05a1 1 0 011 1v7.486l1.793-1.793a1 1 0 111.414 1.414L12 18.364l-4.207-4.207a1 1 0 111.414-1.414L11 14.536V7.05a1 1 0 011-1z" />
         </svg>
