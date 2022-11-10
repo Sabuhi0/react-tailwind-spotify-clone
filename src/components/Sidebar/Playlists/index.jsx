1b0fa027e23881b14375f8485606334138250f62
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-11 01:30:53 +0400

    Update Playlists component

--- src/components/Sidebar/Playlists/index.jsx
+++ src/components/Sidebar/Playlists/index.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Playlists = () => {
   return (
-    <div className=''>
-      Playlists
-    </div>
+    <nav className='mx-6 border-t border-white border-opacity-20 mt-2 py-2 flex-auto overflow-auto'>
+      <ul>
+        {new Array(30).fill(
+          <li>
+            <a href="" className='text-s font-semibold text-link hover:text-active flex h-8 items-center'>Chill Hits</a>
+          </li>
+        )}
+      </ul>
+    </nav>
   )
 }
 
