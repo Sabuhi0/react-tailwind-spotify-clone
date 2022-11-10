81f2a224c150b003acfa895972d3259917db0c69
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-10 22:23:24 +0400

    Add download icon

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -38,6 +38,15 @@ const HeartIcon = ({ size }) => {
     )
 }
 
+const DownloadIcon = ({ size }) => {
+    return (
+        <svg role="img" height={size} width={size} aria-hidden="true" viewBox="0 0 24 24" class="Svg-ytk21e-0 eqtHWV">
+            <path d="M12 3a9 9 0 100 18 9 9 0 000-18zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z" />
+            <path d="M12 6.05a1 1 0 011 1v7.486l1.793-1.793a1 1 0 111.414 1.414L12 18.364l-4.207-4.207a1 1 0 111.414-1.414L11 14.536V7.05a1 1 0 011-1z" />
+        </svg>
+    )
+}
+
 const Icon = ({ name, size = 24 }) => {
 
     const icons = {
@@ -45,7 +54,8 @@ const Icon = ({ name, size = 24 }) => {
         search: SearchIcon,
         collection: CollectionIcon,
         plus: PlusIcon,
-        heart: HeartIcon
+        heart: HeartIcon,
+        download: DownloadIcon
     }
 
     const Component = icons[name]
