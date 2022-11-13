27fe6e066578bfca59655669b1f169bfe977cfd3
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-13 14:38:22 +0400

    Add DownloadApp

--- src/components/Sidebar/DownloadApp/index.jsx
+++ src/components/Sidebar/DownloadApp/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Icon } from '../../../assets/Icons';
 
-const Download = () => {
+const DownloadApp = () => {
   return (
-    <div>
-      
-    </div>
+    <a href='#' className='h-10 flex items-center flex-shrink-0 font-bold text-sm gap-x-4 px-6 transition duration-500 text-link hover:text-active'>
+      <Icon name="download" size={22}/>
+      Install App
+    </a>
   )
 }
 
-export default Download;
+export default DownloadApp;
