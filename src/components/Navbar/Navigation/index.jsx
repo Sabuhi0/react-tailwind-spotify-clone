277b74fd9a00bfde3ad02677c5bcf140e63a5fa3
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-14 07:50:02 +0400

    Update Navigation component

--- src/components/Navbar/Navigation/index.jsx
+++ src/components/Navbar/Navigation/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Icon } from '../../../assets/Icons'
 
 const Navigation = () => {
   return (
-    <nav>
-      Navigation
-    </nav>
+    <div className='flex items-center gap-x-4'>
+      <button className='w-8 h-8 flex items-center justify-center rounded-full bg-black bg-opacity-70'>
+        <Icon name="prev" size={22} />
+      </button>
+      <button className='w-8 h-8 flex items-center justify-center rounded-full bg-black bg-opacity-70'>
+        <Icon name="next" size={22} />
+      </button>
+    </div>
   )
 }
 
