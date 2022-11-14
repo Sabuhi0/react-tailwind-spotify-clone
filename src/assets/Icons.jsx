fbef7e9f9973f34222a7b9eee9d18981b365f0f3
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-14 07:40:00 +0400

    Add next icon

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -55,6 +55,14 @@ const PrevIcon = ({ size }) => {
     )
 }
 
+const NextIcon = ({ size }) => {
+    return (
+        <svg viewBox="0 0 24 24" height={size} width={size} xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+            <path d="M8.043 2.793a1 1 0 000 1.414L15.836 12l-7.793 7.793a1 1 0 101.414 1.414L18.664 12 9.457 2.793a1 1 0 00-1.414 0z" />
+        </svg>
+    )
+}
+
 const Icon = ({ name, size = 24 }) => {
 
     const icons = {
@@ -65,6 +73,7 @@ const Icon = ({ name, size = 24 }) => {
         heart: HeartIcon,
         download: DownloadIcon,
         prev: PrevIcon,
+        next: NextIcon
     }
 
     const Component = icons[name]
