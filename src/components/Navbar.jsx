12f49367e9a8b553bcbc4fcecb28510a250d3781
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-14 06:48:20 +0400

    Passing a Navigation component to a Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navigation from "./Navbar/Navigation";
 
 const Navbar = () => {
     return(
         <nav className="h-16 flex items-center justify-between px-8">
-            Navbar
+            <Navigation />
         </nav>
     )
 }
