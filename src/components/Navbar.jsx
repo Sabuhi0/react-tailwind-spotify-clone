867db42c6abf84bf4cfc2851162e3cb8b21fb953
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-14 06:56:26 +0400

    Passing a Auth component to a Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Auth from "./Navbar/Auth";
 import Navigation from "./Navbar/Navigation";
 
 const Navbar = () => {
     return(
         <nav className="h-16 flex items-center justify-between px-8">
             <Navigation />
+            <Auth />
         </nav>
     )
 }
