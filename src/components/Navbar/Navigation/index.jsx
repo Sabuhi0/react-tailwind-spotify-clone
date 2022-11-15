af38506cbe0b8b0dd0b3e43e7cf8fbf628ca3025
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-15 09:55:02 +0400

    Go back to the previous page with React Router

--- src/components/Navbar/Navigation/index.jsx
+++ src/components/Navbar/Navigation/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 import { Icon } from '../../../assets/Icons'
 
+
 const Navigation = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className='flex items-center gap-x-4'>
-      <button className='w-8 h-8 flex items-center justify-center rounded-full bg-black bg-opacity-70'>
+      <button onClick={() => navigate(-1)} className='w-8 h-8 flex items-center justify-center rounded-full bg-black bg-opacity-70'>
         <Icon name="prev" size={22} />
       </button>
-      <button className='w-8 h-8 flex items-center justify-center rounded-full bg-black bg-opacity-70'>
+      <button onClick={() => navigate(1)} className='w-8 h-8 flex items-center justify-center rounded-full bg-black bg-opacity-70'>
         <Icon name="next" size={22} />
       </button>
     </div>
