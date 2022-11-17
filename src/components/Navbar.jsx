81890d58f71fea293d4eeeabf3af6d5298e3fc90
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-17 16:33:06 +0400

    Update Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import Auth from "./Navbar/Auth";
+import { useLocation } from "react-router-dom";
 import Navigation from "./Navbar/Navigation";
 
 const Navbar = () => {
+
+    const location = useLocation();
+
+    const { pathname } = location
+
+    const searchRoute = pathname.split("/");
+
     return(
         <nav className="h-16 flex items-center justify-between px-8">
             <Navigation />
+
+            {
+                searchRoute[1] === "search" && (
+                    <div>
+                        Search section
+                    </div>
+                )
+            }
+
             <Auth />
         </nav>
     )
