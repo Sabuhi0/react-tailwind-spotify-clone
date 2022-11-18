b383e443f9a0f5deaeb9339506c454a5a3688cc0
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-18 18:55:18 +0400

    Update Navbar componrnt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,24 @@
-import React from "react";
-import Auth from "./Navbar/Auth";
-import { useLocation } from "react-router-dom";
-import Navigation from "./Navbar/Navigation";
+import React from "react"
+import Auth from "./Navbar/Auth"
+import { useLocation } from "react-router-dom"
+import Navigation from "./Navbar/Navigation"
 
 const Navbar = () => {
+  const location = useLocation()
 
-    const location = useLocation();
+  const { pathname } = location
 
-    const { pathname } = location
+  const searchRoute = pathname.split("/")
 
-    const searchRoute = pathname.split("/");
+  return (
+    <nav className="h-16 flex items-center justify-between px-8">
+      <Navigation />
 
-    return(
-        <nav className="h-16 flex items-center justify-between px-8">
-            <Navigation />
+      {searchRoute[1] === "search" && <div>Search section</div>}
 
-            {
-                searchRoute[1] === "search" && (
-                    <div>
-                        Search section
-                    </div>
-                )
-            }
-
-            <Auth />
-        </nav>
-    )
+      <Auth />
+    </nav>
+  )
 }
 
-export default Navbar;
+export default Navbar
