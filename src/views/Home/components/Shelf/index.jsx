d0896c803a5b35a7d93f029192d5ac750de0747c
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-20 01:45:02 +0400

    Update component

--- src/views/Home/components/Shelf/index.jsx
+++ src/views/Home/components/Shelf/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react"
+import { NavLink } from "react-router-dom"
 
-const Shelf = ( {title} ) => {
+const Shelf = ({ title, more = false }) => {
   return (
     <section>
-      <header className='flex items-center justify-between'>
-        <h2 className='text-2xl font-bold'>{title}</h2>
+      <header className="flex items-center justify-between">
+        <h2 className="text-2xl font-bold tracking-tight hover:underline">
+          {title}
+        </h2>
+        {more && (
+          <NavLink
+            className={"text-s font-bold uppercase text-link hover:underline"}
+            to={more}
+          >
+            See All
+          </NavLink>
+        )}
       </header>
     </section>
   )
