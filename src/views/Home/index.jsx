b0d1ed624c27d03a3990ea908067953beb34a63f
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-22 18:31:45 +0400

    Initial data

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -2,11 +2,20 @@ import React from "react"
 import Section from "../Home/components/Shelf/index"
 
 const Home = () => {
+  const items = [
+    {
+      id: 1,
+      title: "Another Love",
+      description: "2013 Tom Odell",
+      image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
+    },
+  ]
   return (
     <div>
       <Section
         title="Your top mixes"
         more="/blabla"
+        items={items}
       />
     </div>
   )
