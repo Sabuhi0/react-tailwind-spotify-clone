eed3cab8e8e713e03ffa6a84876611bc9c1afcbc
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-23 21:00:35 +0400

    Update data

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -9,6 +9,30 @@ const Home = () => {
       description: "2013 Tom Odell",
       image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
     },
+    {
+      id: 2,
+      title: "Sad Songs",
+      description: "Songs for a broken heart",
+      image: "https://i.scdn.co/image/ab67706f00000002583117b5f326c5759bcd4628",
+    },
+    {
+      id: 3,
+      title: "Another Love",
+      description: "2013 Tom Odell",
+      image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
+    },
+    {
+      id: 4,
+      title: "Sad Songs",
+      description: "Songs for a broken heart",
+      image: "https://i.scdn.co/image/ab67706f00000002583117b5f326c5759bcd4628",
+    },
+    // {
+    //   id: 5,
+    //   title: "Another Love",
+    //   description: "2013 Tom Odell",
+    //   image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
+    // },
   ]
   return (
     <div>
