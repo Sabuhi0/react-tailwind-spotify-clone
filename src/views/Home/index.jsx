bf0f08dce4a851a2e924c7b3e291f16ca9eb71ec
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-23 21:57:45 +0400

    Update title

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   const items = [
     {
       id: 1,
-      title: "Another Love",
+      title: "Ice for Aureliano Buendia (Deluxe Edition)",
       description: "2013 Tom Odell",
       image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
     },
