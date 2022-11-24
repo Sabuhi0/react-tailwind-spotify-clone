b520fa89eccba1a62866dcab28b62e8d41b6af1c
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-24 19:19:37 +0400

    Created active home icon

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -6,6 +6,22 @@ const HomeIcon = ({ size }) => {
     )
 }
 
+const ActiveHomeIcon = ({ size }) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      height={size}
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.5 1.515a3 3 0 00-3 0L3 5.845a2 2 0 00-1 1.732V21a1 1 0 001 1h6a1 1 0 001-1v-6h4v6a1 1 0 001 1h6a1 1 0 001-1V7.577a2 2 0 00-1-1.732l-7.5-4.33z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
 const SearchIcon = ({ size }) => {
     return (
         <svg viewBox="0 0 24 24" height={size} width={size} xmlns="http://www.w3.org/2000/svg">
@@ -97,6 +113,7 @@ const Icon = ({ name, size = 24 }) => {
 
     const icons = {
         home: HomeIcon,
+        activeHomeIcon: ActiveHomeIcon,
         search: SearchIcon,
         collection: CollectionIcon,
         plus: PlusIcon,
