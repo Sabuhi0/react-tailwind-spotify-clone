36270c3172fc307395d132499dce39f8818cd1e4
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-24 19:42:04 +0400

    Created active collection icon

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -58,6 +58,22 @@ const CollectionIcon = ({ size }) => {
     )
 }
 
+const ActiveCollectionIcon = ({ size }) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      height={size}
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zM15.5 2.134A1 1 0 0014 3v18a1 1 0 001 1h6a1 1 0 001-1V6.464a1 1 0 00-.5-.866l-6-3.464zM9 2a1 1 0 00-1 1v18a1 1 0 102 0V3a1 1 0 00-1-1z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
 const PlusIcon = ({ size }) => {
     return (
         <svg viewBox="0 0 16 16" height={size} width={size} xmlns="http://www.w3.org/2000/svg">
@@ -137,6 +153,7 @@ const Icon = ({ name, size = 24 }) => {
         search: SearchIcon,
         activeSearchIcon: ActiveSearchIcon,
         collection: CollectionIcon,
+        activeCollectionIcon: ActiveCollectionIcon,
         plus: PlusIcon,
         heart: HeartIcon,
         download: DownloadIcon,
