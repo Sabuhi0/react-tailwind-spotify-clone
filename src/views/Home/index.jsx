7c95e0dfb73b16be262397d9f5d8c60d3f03e042
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-24 17:30:54 +0400

    Add type data

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -8,31 +8,36 @@ const Home = () => {
       title: "Ice for Aureliano Buendia (Deluxe Edition)",
       description: "2013 Tom Odell",
       image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
+      type: "album",
     },
     {
       id: 2,
       title: "Sad Songs",
       description: "Songs for a broken heart",
       image: "https://i.scdn.co/image/ab67706f00000002583117b5f326c5759bcd4628",
+      type: "album",
     },
     {
       id: 3,
-      title: "Another Love",
-      description: "2013 Tom Odell",
-      image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
+      title: "Adele",
+      description: "Artist",
+      image: "https://i.scdn.co/image/ab6761610000f17868f6e5892075d7f22615bd17",
+      type: "artist",
     },
     {
       id: 4,
       title: "Sad Songs",
       description: "Songs for a broken heart",
       image: "https://i.scdn.co/image/ab67706f00000002583117b5f326c5759bcd4628",
+      type: "podcast",
     },
-    // {
-    //   id: 5,
-    //   title: "Another Love",
-    //   description: "2013 Tom Odell",
-    //   image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
-    // },
+    {
+      id: 5,
+      title: "Another Love",
+      description: "2013 Tom Odell",
+      image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
+      type: "album",
+    },
   ]
   return (
     <div>
