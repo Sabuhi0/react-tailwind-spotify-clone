4a41df3746f65032ce681619c6233776a4d83051
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-26 12:14:24 +0400

    Add player next icon

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -171,6 +171,19 @@ const PlayerPrevIcon = ({ size }) => {
   )
 }
 
+const PlayerNextIcon = ({ size }) => {
+  return (
+    <svg
+      viewBox="0 0 16 16"
+      height={size}
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M12.7 1a.7.7 0 00-.7.7v5.15L2.05 1.107A.7.7 0 001 1.712v12.575a.7.7 0 001.05.607L12 9.149V14.3a.7.7 0 00.7.7h1.6a.7.7 0 00.7-.7V1.7a.7.7 0 00-.7-.7h-1.6z" />
+    </svg>
+  )
+}
+
 const Icon = ({ name, size = 24 }) => {
 
     const icons = {
@@ -190,6 +203,7 @@ const Icon = ({ name, size = 24 }) => {
         play: PlayIcon,
         pause: PauseIcon,
         playerPrev: PlayerPrevIcon,
+        playerNext: PlayerNextIcon,
     }
 
     const Component = icons[name]
