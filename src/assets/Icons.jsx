95709221bcb1637e0e06bd907ef5d3f43a760cd4
Sabuhi0 <sabuhiq0@gmail.com>
2022-11-30 00:13:39 +0400

    Add picture in picture icon

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -232,6 +232,22 @@ const QueueIcon = ({ size }) => {
   )
 }
 
+const PictureInPictureIcon = ({ size }) => {
+  return (
+    <svg
+      viewBox="0 0 16 16"
+      height={size}
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g fill="currentColor" fillRule="evenodd">
+        <path d="M1 3v9h14V3H1zm0-1h14a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z" fillRule="nonzero" />
+        <path d="M10 8h4v3h-4z" />
+      </g>
+    </svg>
+  )
+}
+
 const Icon = ({ name, size = 24 }) => {
 
     const icons = {
@@ -256,6 +272,7 @@ const Icon = ({ name, size = 24 }) => {
         shuffle: ShuffleIcon,
         repeat: RepeatIcon,
         queue: QueueIcon,
+        pictureInPicture: PictureInPictureIcon
     }
 
     const Component = icons[name]
