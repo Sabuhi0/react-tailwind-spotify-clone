0f9d429c6f05aee5cae5de7e097a5604bac32bf1
Sabuhi0 <sabuhiq0@gmail.com>
2022-12-02 17:03:28 +0400

    Initial Player component

---
+++ src/components/BottomBar/Player/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Player = () => {
+  return (
+    <div className='flex items-center justify-between px-4'>
+      <div>
+        sol
+      </div>
+      <div>
+        orta
+      </div>
+      <div>
+        sag
+      </div>
+    </div>
+  )
+}
+
+export default Player
