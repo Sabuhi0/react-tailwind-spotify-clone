ea0c88fa6d6bfee29b434c5b3c7e6156981346d8
Sabuhi0 <sabuhiq0@gmail.com>
2022-12-11 15:42:09 +0400

    Add reducer

--- src/stores/index.jsx
+++ src/stores/index.jsx
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import playerReducer from './player'
 
 export default configureStore({
-  reducer: {}
+  reducer: {
+    player: playerReducer
+  }
 })
