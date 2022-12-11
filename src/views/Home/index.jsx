ae7edb5e080041ea982a64e8c7a489894e0f0462
Sabuhi0 <sabuhiq0@gmail.com>
2022-12-11 20:06:15 +0400

    Add artists

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -6,7 +6,8 @@ const Home = () => {
     {
       id: 1,
       title: "Ice for Aureliano Buendia (Deluxe Edition)",
-      description: "Tom Odell",
+      description: "2013 Tom Odell",
+      artist: "Tom Odell",
       image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
       type: "album",
       src: "https://cdn.freesound.org/previews/448/448163_9015615-lq.mp3"
@@ -15,6 +16,7 @@ const Home = () => {
       id: 2,
       title: "Sad Songs",
       description: "Songs for a broken heart",
+      artist: "Sabuhi Gasimov",
       image: "https://i.scdn.co/image/ab67706f00000002583117b5f326c5759bcd4628",
       type: "album",
       src: "https://cdn.freesound.org/previews/173/173561_2069175-lq.mp3"
@@ -23,6 +25,7 @@ const Home = () => {
       id: 3,
       title: "Adele",
       description: "Artist",
+      artist: "Adele Laurie",
       image: "https://i.scdn.co/image/ab6761610000f17868f6e5892075d7f22615bd17",
       type: "artist",
       src: "https://cdn.freesound.org/previews/448/448163_9015615-lq.mp3"
@@ -31,6 +34,7 @@ const Home = () => {
       id: 4,
       title: "Sad Songs",
       description: "Songs for a broken heart",
+      artist: "Sabuhi Gasimov",
       image: "https://i.scdn.co/image/ab67706f00000002583117b5f326c5759bcd4628",
       type: "podcast",
       src: "https://cdn.freesound.org/previews/173/173561_2069175-lq.mp3"
@@ -38,7 +42,8 @@ const Home = () => {
     {
       id: 5,
       title: "Another Love",
-      description: "Tom Odell",
+      description: "2013 Tom Odell",
+      artist: "Tom Odell",
       image: "https://i.scdn.co/image/ab67616d00001e021917a0f3f4152622a040913f",
       type: "album",
       src: "https://cdn.freesound.org/previews/448/448163_9015615-lq.mp3"
