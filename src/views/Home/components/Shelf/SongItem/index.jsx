f34a8d48e161007ae3cd927496f6abc9e0d377b0
Sabuhi0 <sabuhiq0@gmail.com>
2022-12-11 19:35:28 +0400

    Update SongItem component

--- src/views/Home/components/Shelf/SongItem/index.jsx
+++ src/views/Home/components/Shelf/SongItem/index.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { NavLink } from "react-router-dom"
+import { setCurrent } from '../../../../../stores/player'
+import { Icon } from "./../../../../../assets/Icons"
 
 const SongItem = ({ item }) => {
+
+  const dispatch = useDispatch()
+  const { current } = useSelector(state => state.player)
+
+  const imageStyle = item => {
+    switch (item.type) {
+      case "artist":
+        return "rounded-full"
+      case "podcast":
+        return "rounded-lg"
+      default:
+        return "rounded"
+    }
+  }
+
+  const updateCurrent = () => {
+    dispatch(setCurrent(item))
+  }
+
   return (
-    
+    <NavLink
+      key={item.id}
+      to={"/"}
+      className={
+        "bg-footer p-4 rounded hover:bg-[#282828] group transition duration-300 ease-in-out"
+      }
+    >
+      <div className="pt-[100%] relative mb-4">
+        <img
+          src={item.image}
+          className={
+            `absolute inset-0 w-full h-full object-cover ${imageStyle(item)}`
+          }
+        />
+        <button
+          onClick={updateCurrent}
+          className="w-12 h-12 rounded-full bg-primary absolute bottom-2 right-2 group-hover:flex group-hover:duration-700 transition ease-in-out items-center justify-center hidden">
+          <Icon name={current ?.id === item.id ? 'pause' : 'play'} size={current ?.id === item.id ? 20 : 22} />
+        </button>
+      </div>
+      <h1 className="overflow-hidden overflow-ellipsis whitespace-nowrap font-bold">
+        {item.title}
+      </h1>
+      <p className="text-sm text-[#a7a7a7] line-clamp-2 mt-1">
+        {item.description}
+      </p>
+    </NavLink>
   )
 }
 
--- src/views/Home/components/Shelf/index.jsx
+++ src/views/Home/components/Shelf/index.jsx
@@ -1,19 +1,8 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
-import { Icon } from "../../../../assets/Icons"
+import SongItem from "./SongItem"
 
 const Shelf = ({ title, more = false, items }) => {
-
-  const imageStyle = item => {
-    switch (item.type) {
-      case "artist":
-        return "rounded-full"
-      case "podcast":
-        return "rounded-lg"
-      default:
-        return "rounded"
-    }
-  }
 
   return (
     <section className="mb-2">
@@ -31,33 +20,7 @@ const Shelf = ({ title, more = false, items }) => {
         )}
       </header>
       <div className="grid grid-cols-5 gap-x-6">
-        {items.map((item) => (
-          <NavLink
-            key={item.id}
-            to={"/"}
-            className={
-              "bg-footer p-4 rounded hover:bg-[#282828] group transition duration-300 ease-in-out"
-            }
-          >
-            <div className="pt-[100%] relative mb-4">
-              <img
-                src={item.image}
-                className={
-                  `absolute inset-0 w-full h-full object-cover ${imageStyle(item)}`
-                }
-              />
-              <button className="w-12 h-12 rounded-full bg-primary absolute bottom-2 right-2 group-hover:flex group-hover:duration-700 transition ease-in-out items-center justify-center hidden">
-                <Icon name="play" />
-              </button>
-            </div>
-            <h1 className="overflow-hidden overflow-ellipsis whitespace-nowrap font-bold">
-              {item.title}
-            </h1>
-            <p className="text-sm text-[#a7a7a7] line-clamp-2 mt-1">
-              {item.description}
-            </p>
-          </NavLink>
-        ))}
+        {items.map((item) => <SongItem item={item} key={item.id} />)}
       </div>
     </section>
   )
