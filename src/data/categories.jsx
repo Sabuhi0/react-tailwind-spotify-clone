b98dd8135c100ba3e3946d7773b0eed5e3d26d79
Sabuhi0 <sabuhiq0@gmail.com>
2022-12-17 03:26:35 +0400

    Add Data

--- src/data/categories.jsx
+++ src/data/categories.jsx
@@ -1,5 +1,37 @@
 export default [
   {
-    
-  }
+    id: 1,
+    title: '2022 Wrapped',
+    cover: 'https://wrapped-images.spotifycdn.com/image/browse-card/wrapped-temp-card.jpg'
+  },
+  {
+    id: 2,
+    title: 'Podcasts',
+    cover: 'https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5'
+  },
+  {
+    id: 3,
+    title: 'Made For You',
+    cover: 'https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe'
+  },
+  {
+    id: 4,
+    title: 'Pop',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768'
+  },
+  {
+    id: 5,
+    title: 'Pop',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768'
+  },
+  {
+    id: 6,
+    title: 'Pop',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768'
+  },
+  {
+    id: 7,
+    title: 'Mood',
+    cover: 'https://i.scdn.co/image/ab67fb8200005caf271f9d895003c5f5561c1354'
+  },
 ]
