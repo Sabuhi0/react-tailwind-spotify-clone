c2c43d1f425813671d6b159c4abe2869932b2e55
Sabuhi0 <sabuhiq0@gmail.com>
2022-12-22 15:59:17 +0400

    Update Search

--- src/components/Navbar/Search/index.jsx
+++ src/components/Navbar/Search/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Icon } from '../../../assets/Icons'
 
 const Search = () => {
   return (
-    <div>
-      Search
+    <div className='mr-auto ml-4 relative'>
+      <label htmlFor="search-input" className="w-12 h-10 flex items-center justify-center absolute top-0 left-0">
+        <Icon name='search' />
+      </label>
+      <input type="text" className='h-10 w-[22.75rem] max-w-full text-black text-sm rounded-[500px] px-12 py-[6px] focus:outline-none placeholder-black/50' placeholder='What do you want to listen to?' />
     </div>
   )
 }
