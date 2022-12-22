86aac279d7cee0b032327f154598e91de9d80309
Sabuhi0 <sabuhiq0@gmail.com>
2022-12-22 11:34:45 +0400

    Add colors

--- src/data/categories.jsx
+++ src/data/categories.jsx
@@ -2,36 +2,43 @@ export default [
   {
     id: 1,
     title: '2022 Wrapped',
-    cover: 'https://wrapped-images.spotifycdn.com/image/browse-card/wrapped-temp-card.jpg'
+    cover: 'https://wrapped-images.spotifycdn.com/image/browse-card/wrapped-temp-card.jpg',
+    color: '#8400e7'
   },
   {
     id: 2,
     title: 'Podcasts',
-    cover: 'https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5'
+    cover: 'https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5',
+    color: '#e13300'
   },
   {
     id: 3,
     title: 'Made For You',
-    cover: 'https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe'
+    cover: 'https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe',
+    color: '#1e3264'
   },
   {
     id: 4,
     title: 'Pop',
-    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768'
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#e8115b'
   },
   {
     id: 5,
     title: 'Pop',
-    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768'
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#148a08'
   },
   {
     id: 6,
     title: 'Pop',
-    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768'
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#bc5900'
   },
   {
     id: 7,
     title: 'Mood',
-    cover: 'https://i.scdn.co/image/ab67fb8200005caf271f9d895003c5f5561c1354'
+    cover: 'https://i.scdn.co/image/ab67fb8200005caf271f9d895003c5f5561c1354',
+    color: '#e1118c'
   },
 ]
