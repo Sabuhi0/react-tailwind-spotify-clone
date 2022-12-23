61968c1ef89d00ae29164bda5363a52ffcdc6b3f
Sabuhi0 <sabuhiq0@gmail.com>
2022-12-23 23:39:33 +0400

    Update data

--- src/data/categories.jsx
+++ src/data/categories.jsx
@@ -41,4 +41,94 @@ export default [
     cover: 'https://i.scdn.co/image/ab67fb8200005caf271f9d895003c5f5561c1354',
     color: '#e1118c'
   },
+  {
+    id: 8,
+    title: 'Made For You',
+    cover: 'https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe',
+    color: '#1e3264'
+  },
+  {
+    id: 9,
+    title: 'New releases',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#e8115b'
+  },
+  {
+    id: 10,
+    title: 'Pop',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#148a08'
+  },
+  {
+    id: 11,
+    title: 'Hip-Hop',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#bc5900'
+  },
+  {
+    id: 12,
+    title: '2022 Wrapped',
+    cover: 'https://wrapped-images.spotifycdn.com/image/browse-card/wrapped-temp-card.jpg',
+    color: '#8400e7'
+  },
+  {
+    id: 13,
+    title: 'Podcasts',
+    cover: 'https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5',
+    color: '#e13300'
+  },
+  {
+    id: 14,
+    title: 'Made For You',
+    cover: 'https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe',
+    color: '#1e3264'
+  },
+  {
+    id: 15,
+    title: 'New releases',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#e8115b'
+  },
+  {
+    id: 16,
+    title: 'Pop',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#148a08'
+  },
+  {
+    id: 17,
+    title: 'Hip-Hop',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#bc5900'
+  },
+  {
+    id: 18,
+    title: 'Mood',
+    cover: 'https://i.scdn.co/image/ab67fb8200005caf271f9d895003c5f5561c1354',
+    color: '#e1118c'
+  },
+  {
+    id: 19,
+    title: 'Made For You',
+    cover: 'https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe',
+    color: '#1e3264'
+  },
+  {
+    id: 20,
+    title: 'New releases',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#e8115b'
+  },
+  {
+    id: 21,
+    title: 'Pop',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#148a08'
+  },
+  {
+    id: 22,
+    title: 'Hip-Hop',
+    cover: 'https://i.scdn.co/image/ab67fb8200005cafa862ab80dd85682b37c4e768',
+    color: '#bc5900'
+  },
 ]
