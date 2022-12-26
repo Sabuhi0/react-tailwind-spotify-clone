0c4f4eb09dcdfb85ef78a1bddc91c1e27759a5ec
Sabuhi0 <sabuhiq0@gmail.com>
2022-12-26 17:04:34 +0400

    Update Title component

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-const Title = ({}) => {
+const Title = ({title, more = false}) => {
   return (
-    <div>
-      
-    </div>
+    <header className="flex items-center justify-between mb-4">
+      <h2 className="text-2xl font-bold tracking-tight hover:underline">
+        <NavLink to={more ?? '#'}>{title}</NavLink>
+      </h2>
+      {more && (
+        <NavLink
+          className={"text-s font-bold uppercase text-link hover:underline tracking-wider"}
+          to={more}
+        >
+          See All
+        </NavLink>
+      )}
+    </header>
   )
 }
 
