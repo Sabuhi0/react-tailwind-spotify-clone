1cc0d474d2b9632a3322a222d49383c8c43d012f
Sabuhi0 <sabuhiq0@gmail.com>
2022-12-26 17:05:45 +0400

    Add title component

--- src/views/Home/components/Shelf/index.jsx
+++ src/views/Home/components/Shelf/index.jsx
@@ -1,24 +1,13 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
+import Title from "../../../../components/Title"
 import SongItem from "./SongItem"
 
 const Shelf = ({ title, more = false, items }) => {
 
   return (
     <section className="mb-2">
-      <header className="flex items-center justify-between mb-4">
-        <h2 className="text-2xl font-bold tracking-tight hover:underline">
-          <NavLink to={more ?? '#'}>{title}</NavLink>
-        </h2>
-        {more && (
-          <NavLink
-            className={"text-s font-bold uppercase text-link hover:underline tracking-wider"}
-            to={more}
-          >
-            See All
-          </NavLink>
-        )}
-      </header>
+      <Title title={title} more={more} />
       <div className="grid grid-cols-5 gap-x-6">
         {items.map((item) => <SongItem item={item} key={item.id} />)}
       </div>
