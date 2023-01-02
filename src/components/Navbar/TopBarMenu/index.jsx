0382a4ff5f4c164cc214d280921f0e1545ab1f00
Sabuhi0 <sabuhiq0@gmail.com>
2023-01-02 18:55:12 +0400

    Update Tob Bar Menu

--- src/components/Navbar/TopBarMenu/index.jsx
+++ src/components/Navbar/TopBarMenu/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const TopBarMenu = () => {
   return (
-    <></>
+    <div className='mr-auto pl-6'>
+      <nav className=''>
+        <ul className='flex items-center'>
+          <li><NavLink to={''} className='hover:bg-[#333] px-4 py-2 inline-block rounded-[4px] text-sm font-bold'>Playlists</NavLink></li>
+          <li><NavLink to={''} className='hover:bg-[#333] px-4 py-2 inline-block rounded-[4px] text-sm font-bold'>Podcasts</NavLink></li>
+          <li><NavLink to={''} className='hover:bg-[#333] px-4 py-2 inline-block rounded-[4px] text-sm font-bold'>Artists</NavLink></li>
+          <li><NavLink to={''} className='hover:bg-[#333] px-4 py-2 inline-block rounded-[4px] text-sm font-bold'>Albums</NavLink></li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
